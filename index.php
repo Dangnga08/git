f6b89603d0bbd0edb083171bcc08d929a9f26cfe
Dangnga08 <38774707+Dangnga08@users.noreply.github.com>
2018-04-28 09:30:35 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+echo " Hello";
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta name="author" content="ABCDE" />
+    <meta charset= "UTF-8" />
+	<title> Giới thiệu bản thân </title>
+</head>
+
+<body>
+<p> Xin chào mọi người <br />
+Tôi tên là Nga. Tôi 22 tuổi. Tôi đến từ Thái Bình. Hiện tôi đang học Khoa Hệ thống thông tin tại Trường Học viện Ngân hàng.<br />
+ Tôi rất thích nghe nhạc, xem phim và đi du lịch.Tôi rất sợ ma và côn trùng. Tôi cũng đặc biệt yêu thích công nghệ nhất là những thứ liên quan đến khoa học máy tính <br />
+ <img src="img/ảnh.jpg"/>
+</p>
+<p> Năm 2010- 2014: Tôi học trường THPT Vũ Tiên- Thái Bình <br />
+    Năm 2014- 2018: Tôi học Học viện Ngân hàng <br />
+    Tháng 6 - 2018: Tốt nghiệp đại học</p>
+
+
+</body>
+</html>
